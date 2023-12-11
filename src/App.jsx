38b5296b4930cd3ff6615f0069ac35744d30cfbe
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-12-11 15:46:54 +0300

    feat: setup basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
+
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { AddEvent } from './pages/AddEvent';
+
 function App() {
-	return <>Home</>;
+	return (
+		<>
+			{/* Navbar */}
+			<Navbar />
+
+			<Container>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+
+					<Route path="add-event" element={<AddEvent />} />
+				</Routes>
+			</Container>
+		</>
+	);
 }
 
 export default App;
