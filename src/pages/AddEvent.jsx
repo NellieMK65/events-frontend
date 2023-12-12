8d1725917f29963a3e826776d36d09ab219e259c
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-12-12 14:55:07 +0300

    event form done

--- src/pages/AddEvent.jsx
+++ src/pages/AddEvent.jsx
@@ -15,10 +15,13 @@ import {
 	Textarea,
 } from '@chakra-ui/react';
 
+import { BASE_URL } from '../utils';
+
 export const AddEvent = () => {
 	const initialData = {
 		title: '',
 		description: '',
+		image: '',
 		location: '',
 		price: '',
 		start_date: '',
@@ -26,6 +29,7 @@ export const AddEvent = () => {
 	};
 
 	const [formData, setFormData] = useState(initialData);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleChange = (e) => {
 		setFormData({
@@ -34,20 +38,53 @@ export const AddEvent = () => {
 		});
 	};
 
-	console.log(formData);
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+
+		setIsLoading(true);
+
+		fetch(`${BASE_URL}/events`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(formData),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				// we assume the operation was a success
+
+				// reset form
+				setFormData(initialData);
+
+				// stop loading
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				console.log(err);
+			});
+	};
 
 	return (
 		<Flex align={'center'} justify={'center'}>
 			<Stack>
 				<Heading fontSize={'4xl'}>Add Event</Heading>
 
-				<Box rounded={'lg'} bg={'white'} p={8}>
+				<Box
+					as="form"
+					rounded={'lg'}
+					bg={'white'}
+					p={8}
+					onSubmit={handleFormSubmit}
+				>
 					<Stack>
 						<FormControl isRequired>
 							<FormLabel>Title</FormLabel>
 							<Input
 								name="title"
 								placeholder="Hiking"
+								required
 								value={formData['title']}
 								onChange={handleChange}
 							/>
@@ -58,7 +95,19 @@ export const AddEvent = () => {
 							<Textarea
 								name="description"
 								placeholder="description"
+								required
 								value={formData['description']}
+								onChange={handleChange}
+							/>
+						</FormControl>
+
+						<FormControl isRequired>
+							<FormLabel>Image</FormLabel>
+							<Input
+								name="image"
+								placeholder="image"
+								required
+								value={formData['image']}
 								onChange={handleChange}
 							/>
 						</FormControl>
@@ -67,6 +116,7 @@ export const AddEvent = () => {
 							<FormLabel>Location</FormLabel>
 							<Input
 								name="location"
+								required
 								placeholder="Ngong hills"
 								value={formData['location']}
 								onChange={handleChange}
@@ -87,6 +137,7 @@ export const AddEvent = () => {
 								<Input
 									type="number"
 									name="price"
+									required
 									placeholder="100"
 									value={formData['price']}
 									onChange={handleChange}
@@ -100,6 +151,7 @@ export const AddEvent = () => {
 								<Input
 									name="start_date"
 									type="datetime-local"
+									required
 									value={formData['start_date']}
 									onChange={handleChange}
 								/>
@@ -109,6 +161,7 @@ export const AddEvent = () => {
 								<Input
 									name="end_date"
 									type="datetime-local"
+									required
 									value={formData['end_date']}
 									onChange={handleChange}
 								/>
@@ -116,7 +169,14 @@ export const AddEvent = () => {
 						</HStack>
 
 						<Stack pt={3}>
-							<Button>Submit</Button>
+							<Button
+								isLoading={isLoading}
+								loadingText="Saving event"
+								type="submit"
+								colorScheme="teal"
+							>
+								Submit
+							</Button>
 						</Stack>
 					</Stack>
 				</Box>
