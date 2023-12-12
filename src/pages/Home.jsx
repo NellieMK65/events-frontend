7058e435af38e8ed517a3d6f80e2a5aaa66c197b
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-12-12 15:42:10 +0300

    finish working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import { Center, Flex, Heading, Input, SimpleGrid } from '@chakra-ui/react';
+
+import { EventCard } from '../components/EventCard';
+import { BASE_URL } from '../utils';
+
+const data = [
+	{
+		id: 1,
+		title: 'Hiking',
+		description: 'Enjoy scenic views',
+		image: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Katara.png',
+		location: 'Ngong',
+		price: 200,
+		start_date: 'Dec 12, 2023',
+		end_date: 'Dec, 15, 2023',
+	},
+];
+
 export const Home = () => {
-	return <div>Home</div>;
+	const [events, setEvents] = useState(data);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	useEffect(() => {
+		const handleFetchData = () => {
+			fetch(`${BASE_URL}/events`)
+				.then((res) => res.json())
+				.then((events) => setEvents(events))
+				.catch((err) => console.log(err));
+		};
+
+		handleFetchData();
+	}, []);
+
+	const searchedEvents = events.filter((event) =>
+		event.title.toLowerCase().includes(searchTerm.toLowerCase())
+	);
+
+	return (
+		<Flex direction="column" p={4} gap={4}>
+			<Input
+				placeholder="Search"
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+
+			{/* Display events */}
+			{searchedEvents.length > 0 ? (
+				<SimpleGrid columns={{ sm: 1, md: 3, lg: 4 }} spacing={5}>
+					{searchedEvents.map((event, index) => (
+						<EventCard key={index} {...event} />
+					))}
+				</SimpleGrid>
+			) : (
+				<Center>
+					<Heading size={'4xl'}>No events</Heading>
+				</Center>
+			)}
+		</Flex>
+	);
 };
